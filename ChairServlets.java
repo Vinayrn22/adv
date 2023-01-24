d4e5081542a3ee35094b0dbff2a0a19445f49038
Vinayrn22 <113450949+Vinayrn22@users.noreply.github.com>
2023-01-24 11:35:26 +0530

    Add files via upload

---
+++ ChairServlets.java
@@ -0,0 +1,25 @@
+package com.xworkz;
+
+import java.io.IOException;
+
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(loadOnStartup = 1 , urlPatterns ="/chair")
+public class ChairServlets extends HttpServlet {
+	
+	public void LaptopServelets() 
+	{
+		System.out.println("laptop"+getClass().getSimpleName());	
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		System.out.println("running chair");
+	}
+}
